250b062013d2461096db22daef591c626cc60aa8
VetriRex <kvetri944@gmail.com>
2024-11-20 11:06:14 +0530

    first commit

---
+++ ourservices.js
@@ -0,0 +1,15 @@
+// import ImageCompare from 'image-compare-viewer';
+// import 'image-compare-viewer/dist/image-compare-viewer.css';
+
+
+// // document.addEventListener('DOMContentLoaded', () => {
+// //     const container = document.getElementById('image-compare-container');
+// //     const viewer = new ImageCompare(container, {
+// //       // Options
+// //       controlColor: '#fff', // Slider control color
+// //       controlShadow: true, // Adds shadow to the control
+// //       addCircle: true, // Adds a circle to the control
+// //       smoothing: true, // Enables smoothing
+// //     });
+// //     viewer.mount(); // Initialize the slider
+// //   });
