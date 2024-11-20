214c954e92ea918127fbeac804b59c5c9c6f03c7
VetriRex <kvetri944@gmail.com>
2024-11-20 19:38:53 +0530

    first commit

--- ourservices.js
+++ ourservices.js
@@ -1,15 +1,24 @@
-// import ImageCompare from 'image-compare-viewer';
-// import 'image-compare-viewer/dist/image-compare-viewer.css';
-
-
-// // document.addEventListener('DOMContentLoaded', () => {
-// //     const container = document.getElementById('image-compare-container');
-// //     const viewer = new ImageCompare(container, {
-// //       // Options
-// //       controlColor: '#fff', // Slider control color
-// //       controlShadow: true, // Adds shadow to the control
-// //       addCircle: true, // Adds a circle to the control
-// //       smoothing: true, // Enables smoothing
-// //     });
-// //     viewer.mount(); // Initialize the slider
-// //   });
+document.querySelectorAll('.faq-question').forEach((question) => {
+    question.addEventListener('click', () => {
+      const answer = question.nextElementSibling;
+      const arrow = question.querySelector('.arrow');
+      const isActive = answer.classList.contains('active');
+      
+      if (isActive) {
+        arrow.style.transform = 'rotate(0deg)';
+        answer.style.maxHeight = null;
+        answer.classList.remove('active');
+  
+        // Delay removing the active class from the question for smooth transition
+        setTimeout(() => {
+          question.classList.remove('active'); // Remove active class after answer closes
+        }, 500); // Ensure this time matches the answer's transition duration
+      } else {
+        arrow.style.transform = 'rotate(180deg)';
+        answer.style.maxHeight = `${answer.scrollHeight}px`;
+        answer.classList.add('active');
+        question.classList.add('active'); // Add active class to question immediately
+      }
+    });
+  });
+  
